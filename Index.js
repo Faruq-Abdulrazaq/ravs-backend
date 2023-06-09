93564fcce5bc0e68cfe807e4b89119f514c001f4
Faruq Abdulrazaq <abdulrazaq556@gmail.com>
2023-06-09 22:35:37 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -12,7 +12,7 @@ initializeApp({
 
 const db = getFirestore();
 app.use(express.json());
-app.use(cors({ origin: 'http://localhost:3000' , credentials :  true}));
+app.use(cors({ origin: 'https://ravs-53992.firebaseapp.com/' , credentials :  true}));
 app.listen(PORT, () => console.log(`server running on PORT ${PORT}`))
 
 app.get('/', (req, res) => {
